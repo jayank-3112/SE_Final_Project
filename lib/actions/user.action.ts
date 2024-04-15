66877892429287407b6ac4db195769e6f1634179
jayank-3112 <guptajayank@gmail.com>
2024-04-15 22:36:05 +0530

    mongo db integrated

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,16 @@
+"user server";
+
+import User from "@/lib/modals/user.modal"
+
+import { connect } from "@/lib/db"
+import { stringify } from "querystring";
+
+export async function createUser(user: any) {
+    try {
+        await connect();
+        const newUser = await User.create(user);
+        return JSON.parse (JSON.stringify(newUser));
+    } catch (error) {
+        console.log(error);
+    }
+}
